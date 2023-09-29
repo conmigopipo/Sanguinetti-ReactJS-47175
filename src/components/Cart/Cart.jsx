406c0d3aed0b3050025aa36c65537026b3396f20
conmigopipo <felipesanguinettip@gmail.com>
2023-09-29 14:17:45 -0300

    pasreInt fix on cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,12 +3,13 @@ import { useCartContext } from "../../context/cartContext"
 const Cart = ({disco}) => {
 
     const {eliminarProducto} = useCartContext()
-
+    console.log(disco)
+    console.log(disco.precio)
     return (
                 <tr>
                     <th scope="row" >{disco.titulo}</th>
                     <th>{disco.autor}</th>
-                    <td>{disco.precio.toFixed(2)}</td>
+                    <td>{parseInt(disco.precio).toFixed(2)}</td>
                     <td>{disco.quantity}</td>
                     <td><button className="btn btn-outline-danger" onClick={()=> eliminarProducto(disco.id)}>X</button></td>
                 </tr>
